e1abba7cfce18a4b28c27294bc67d975158505fb
Yunseo Hwang <yunseo.dev@gmail.com>
2021-03-17 23:15:01 +0900

    Update users table
    Signed-off-by: Yunseo Hwang <yunseo.dev@gmail.com>

--- postgres/users.sql
+++ postgres/users.sql
@@ -1,11 +1,14 @@
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS users_login;
+DROP TABLE IF EXISTS users_avatar;
+
 BEGIN TRANSACTION;
 
 CREATE TABLE users (
   id SERIAL NOT NULL PRIMARY KEY,
   username VARCHAR(100),
-  userid TEXT UNIQUE NOT NULL,
-  userlevel BIGINT DEFAULT 1,
-  exp BIGINT DEFAULT 0,
+  email TEXT UNIQUE NOT NULL,
+  experience BIGINT DEFAULT 0,
   postcnt BIGINT DEFAULT 0,
   quizcnt BIGINT DEFAULT 0,
   joined TIMESTAMP NOT NULL
@@ -13,14 +16,14 @@ CREATE TABLE users (
 
 CREATE TABLE users_login (
   id SERIAL NOT NULL PRIMARY KEY,
-  userhash VARCHAR(100) NOT NULL,
-  userid TEXT UNIQUE NOT NULL
+  email TEXT UNIQUE NOT NULL
+  password VARCHAR(100) NOT NULL,
 );
 
 CREATE TABLE users_avatar (
   id SERIAL NOT NULL PRIMARY KEY,
-  users_id TEXT UNIQUE NOT NULL,
-  avatarfilename TEXT,
+  email TEXT UNIQUE NOT NULL,
+  filename TEXT,
   filepath TEXT,
   mimetype TEXT,
   size BIGINT
