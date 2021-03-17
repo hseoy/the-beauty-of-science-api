c26afd18fa5e58992a4014724f0168faf3e7a3c8
Yunseo Hwang <yunseo.dev@gmail.com>
2021-03-17 23:29:54 +0900

    Fix typo
    Signed-off-by: Yunseo Hwang <yunseo.dev@gmail.com>

--- postgres/users.sql
+++ postgres/users.sql
@@ -16,8 +16,8 @@ CREATE TABLE users (
 
 CREATE TABLE users_login (
   id SERIAL NOT NULL PRIMARY KEY,
-  email TEXT UNIQUE NOT NULL
-  password VARCHAR(100) NOT NULL,
+  email TEXT UNIQUE NOT NULL,
+  password VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE users_avatar (
