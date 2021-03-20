86b4523e2dc9fa4e54f5e29aece12b4135b05795
Yunseo Hwang <yunseo.dev@gmail.com>
2021-03-20 22:45:38 +0900

    Refactor AuthService
    Signed-off-by: Yunseo Hwang <yunseo.dev@gmail.com>

--- src/services/auth.js
+++ src/services/auth.js
@@ -16,10 +16,14 @@ export default class AuthService {
     try {
       await this.userModel.createUserLogin(email, hashedPw);
       [result.user] = await this.userModel.createUser(email, username);
-      const token = this.authHelper.generateToken(result.user);
-      await this.authHelper.storeRefreshToken(token.refresh, email);
+
+      const access = this.authHelper.generateAccessToken(result.user);
+      const refresh = this.authHelper.generateRefreshToken();
+      result.token = { access, refresh };
+
+      await this.authHelper.storeRefreshToken(refresh, email);
+
       await this.userModel.transactionCommit();
-      result.token = token;
     } catch (e) {
       await this.userModel.transactionRollback();
       console.log(e);
@@ -36,11 +40,16 @@ export default class AuthService {
 
       if (isValid) {
         const [user] = await this.userModel.findByEmail(email);
-        const { access, refresh } = this.authHelper.generateToken(user);
+
+        const access = this.authHelper.generateAccessToken(user);
+        const refresh = this.authHelper.generateRefreshToken();
+
         await this.authHelper.storeRefreshToken(refresh, email);
+
         return { user, token: { access, refresh } };
       }
     } catch (e) {
+      console.error(e);
       throw createError(400, 'unable to signin');
     }
     return null;
@@ -54,17 +63,20 @@ export default class AuthService {
     }
   }
 
-  async RefreshAccessToken(token, email) {
-    const [user] = await this.userModel.findByEmail(email);
-    const isValid =
-      user && (await this.authHelper.verifyRefreshToken(token, email));
-    if (isValid) {
-      const { access } = this.authHelper.generateToken(
-        { email },
-        { access: true, refresh: false },
-      );
-      return { access };
+  async RefreshAccessToken(refreshToken, accessToken) {
+    try {
+      const { email } = this.authHelper.decodeAccessToken(accessToken);
+      const [user] = await this.userModel.findByEmail(email);
+      const isValid =
+        user && (await this.authHelper.verifyRefreshToken(refreshToken, email));
+      if (isValid) {
+        const access = this.authHelper.generateAccessToken(user);
+        return { access };
+      }
+    } catch (e) {
+      console.error(e);
+      throw createError(401, 'Unauthorized');
     }
-    throw createError(401, 'Unauthorized');
+    return null;
   }
 }
