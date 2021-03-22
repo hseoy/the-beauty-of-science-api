0514155bd9aef4e4bcde3d1b8de0a37392ceef18
Yunseo Hwang <yunseo.dev@gmail.com>
2021-03-22 09:38:12 +0900

    Add attachCurrentUser middleware
    Signed-off-by: Yunseo Hwang <yunseo.dev@gmail.com>

---
+++ src/api/middlewares/attachCurrentUser.js
@@ -0,0 +1,18 @@
+import { Container } from 'typedi';
+
+const attachCurrentUser = async (req, res, next) => {
+  try {
+    const userModel = Container.get('userModel');
+    const [user] = await userModel.findById(req.token.id);
+    if (!user) {
+      return res.sendStatus(401);
+    }
+    req.currentUser = user;
+    return next();
+  } catch (e) {
+    console.log(e);
+    return next(e);
+  }
+};
+
+export default attachCurrentUser;
--- src/api/middlewares/index.js
+++ src/api/middlewares/index.js
@@ -1,10 +1,12 @@
 import { isRefreshToken, isAccessToken } from './isJwtToken';
 import isAuth from './isAuth';
+import attachCurrentUser from './attachCurrentUser';
 
 const middlewares = {
   isAuth,
   isRefreshToken,
   isAccessToken,
+  attachCurrentUser,
 };
 
 export default middlewares;
